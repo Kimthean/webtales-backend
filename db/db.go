c30d0318ade84631c57013c79b441de9cd647926
Thean <theantian089@gmail.com>
2024-08-02 09:39:33 +0700

    chore: Refactor database initialization and configuration

--- db/db.go
+++ db/db.go
@@ -12,46 +12,40 @@ import (
 )
 
 func InitDB(databaseURL string) (*gorm.DB, error) {
-	// Create a custom logger
+
 	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
 		logger.Config{
-			SlowThreshold:             time.Second, // Log slow queries
-			LogLevel:                  logger.Warn, // Log level
-			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
-			Colorful:                  false,       // Disable color
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Warn,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  false,
 		},
 	)
 
-	// Open database connection
 	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{
 		Logger:                 newLogger,
-		PrepareStmt:            true, // Enable prepared statement cache
-		SkipDefaultTransaction: true, // Skip default transaction for performance
+		PrepareStmt:            true,
+		SkipDefaultTransaction: true,
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	// Auto Migrate the schema
 	err = db.AutoMigrate(&models.Novel{}, &models.Chapter{})
 	if err != nil {
 		return nil, err
 	}
 
-	// Get generic database object sql.DB to use its functions
 	sqlDB, err := db.DB()
 	if err != nil {
 		return nil, err
 	}
 
-	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
 	sqlDB.SetMaxIdleConns(10)
 
-	// SetMaxOpenConns sets the maximum number of open connections to the database.
 	sqlDB.SetMaxOpenConns(100)
 
-	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
 	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	return db, nil
